55c414ab1d6988f801e7ab79d0e6586d524067ac
Lil-Noe <lilian.noe@epfl.ch>
2025-06-03 21:58:23 +0200

    feat: add message passing to client

--- lab06/src/amyc/lsp/LanguageServer.scala
+++ lab06/src/amyc/lsp/LanguageServer.scala
@@ -1,32 +1,56 @@
 package amyc.lsp
 
 import java.util.concurrent.CompletableFuture
-import org.eclipse.lsp4j.services.LanguageServer
 import org.eclipse.lsp4j.InitializeParams
 import org.eclipse.lsp4j.InitializeResult
 import org.eclipse.lsp4j.ServerCapabilities
 import org.eclipse.lsp4j.services.WorkspaceService
 import org.eclipse.lsp4j.services.TextDocumentService
 import org.eclipse.lsp4j.services.LanguageClient
+import org.eclipse.lsp4j.services.LanguageServer
 import org.eclipse.lsp4j.services.LanguageClientAware
 import org.eclipse.lsp4j.TextDocumentSyncKind
+import org.eclipse.lsp4j.MessageParams
+import org.eclipse.lsp4j.MessageType
 
+/**
+  * This Server receives requests/notifications from a client 
+  * to perform certain operations regarding Amy language.
+  */
 class AmyLanguageServer extends LanguageServer with LanguageClientAware {
+    
+    // Language client, initialized when launching
+    private var languageClient: LanguageClient = null
+    // Connect to a client, called when launching
+    override def connect(client: LanguageClient): Unit = { languageClient = client }
+    
 
     // Initialize the workspace and text document services
     private val workspaceService: WorkspaceService = new AmyWorkspaceService()
     private val textDocumentService: TextDocumentService = new AmyTextDocumentService()
-    
-    // Initialize the language client
-    private var languageClient: LanguageClient = null
-    
-    
-    // Implement the LanguageClientAware and LanguageServer interfaces
 
     override def getWorkspaceService(): WorkspaceService = workspaceService
-
     override def getTextDocumentService(): TextDocumentService = textDocumentService
 
+
+    // Exit the process
+    override def exit(): Unit = System.exit(0)
+
+
+    // Shutdown server (not exit to get response)
+    override def shutdown(): CompletableFuture[Object] = CompletableFuture.completedFuture(null)
+
+
+    // Utility method to send messages from Server on Client side
+    // (used to debug and control process)
+    def sendMsgToClient(message: String): Unit = {
+        val params: MessageParams = new MessageParams(MessageType.Info, message)
+        languageClient.showMessage(params)
+    }
+    
+
+    // Initialize the process
+    // (first request the server should receive)
     override def initialize(params: InitializeParams): CompletableFuture[InitializeResult] = {
 
         // Create the capabilities of the server
@@ -44,11 +68,5 @@ class AmyLanguageServer extends LanguageServer with LanguageClientAware {
         // Return the CompletableFuture
         CompletableFuture.completedFuture(result)
     }
-    
-    override def connect(client: LanguageClient): Unit = this.languageClient = client
-
-    override def exit(): Unit = System.exit(0)
-    
-    override def shutdown(): CompletableFuture[Object] = CompletableFuture.completedFuture(null)
 
 }
